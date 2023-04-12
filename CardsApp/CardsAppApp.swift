eb92b788c47243605c6f8f8904a764d9ec004748
Anatolii-sx <s8.8@bk.ru>
2023-04-12 17:34:32 +0300

    Initial Commit

---
+++ CardsApp/CardsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsAppApp.swift
+//  CardsApp
+//
+//  Created by Анатолий Миронов on 12.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CardsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
